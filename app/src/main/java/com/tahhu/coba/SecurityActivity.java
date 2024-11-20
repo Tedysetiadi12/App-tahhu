6466229d804bfea41e5ebfb402366d9fd8ab74d6
Tedysetiadi12 <tedisetiadi744@gmail.com>
2024-11-20 21:37:21 +0700

    Update layout security

--- app/src/main/java/com/tahhu/coba/SecurityActivity.java
+++ app/src/main/java/com/tahhu/coba/SecurityActivity.java
@@ -3,6 +3,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class SecurityActivity extends AppCompatActivity {
     @Override
@@ -12,6 +19,20 @@ public class SecurityActivity extends AppCompatActivity {
 
         findViewById(R.id.btn_office_security).setOnClickListener(v -> openDurationActivity("Pengamanan Kantor"));
         findViewById(R.id.btn_personal_security).setOnClickListener(v -> openDurationActivity("Pengamanan Pribadi"));
+        ViewPager2 bannerViewPager = findViewById(R.id.bannerViewPager);
+        TabLayout dotsIndicator = findViewById(R.id.dotsIndicator);
+
+        List<Integer> banners = Arrays.asList(
+                R.drawable.banner1,
+                R.drawable.benner1
+        );
+
+        BannerAdapter bannerAdapter = new BannerAdapter(banners);
+        bannerViewPager.setAdapter(bannerAdapter);
+
+        new TabLayoutMediator(dotsIndicator, bannerViewPager, (tab, position) -> {
+            // No specific text needed for dots
+        }).attach();
     }
 
     private void openDurationActivity(String securityType) {
